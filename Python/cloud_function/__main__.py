51fac17c15f7b32c1ee944e9dd4d012776cf7497
Torsten Steinbach <torsten@de.ibm.com>
2018-02-27 17:33:49 +0100

    Update __main__.py

--- Python/cloud_function/__main__.py
+++ Python/cloud_function/__main__.py
@@ -29,27 +29,10 @@ def main(args):
     sql_statement_text = args.get("sql", "")
     if sql_statement_text == "":
         return {'error': 'No SQL statement specified'}
-    #ibmcloud_apikey = 'YNiXREBMTfQsSzKrXYhQJElNXnUFEgpQ7qVVTkDK3_Zr'
-    #sql_instance_crn = 'crn%3Av1%3Abluemix%3Apublic%3Asql-query%3Aus-south%3Aa%2Fd86af7367f70fba4f306d3c19c938f2f%3Ad1b2c005-e3d8-48c0-9247-e9726a7ed510%3A%3A'
-    #target_endpoint = 's3.us-south.objectstorage.softlayer.net'
-    #target_bucket = 'sqltempregional'
     sqlClient = ibmcloudsql.SQLQuery(ibmcloud_apikey, sql_instance_crn, target_endpoint, target_bucket,
                                      target_cos_prefix=target_prefix, client_info=client_infofmation)
     sqlClient.logon()
     jobId = sqlClient.submit_sql(sql_statement_text)
-    #jobId = sqlClient.submit_sql(
-    #    "WITH orders_shipped AS \
-    #      (SELECT OrderID, EmployeeID, (CASE WHEN shippedDate < requiredDate \
-    #                                       THEN 'On Time' \
-    #                                       ELSE 'Late' \
-    #                                    END) AS Shipped \
-    #       FROM cos://us-geo/sql/orders.parquet STORED AS PARQUET) \
-    #    SELECT e.FirstName, e.LastName, COUNT(o.OrderID) As NumOrders, Shipped \
-    #    FROM orders_shipped o, \
-    #         cos://us-geo/sql/employees.parquet STORED AS PARQUET e \
-    #    WHERE e.EmployeeID = o.EmployeeID \
-    #    GROUP BY e.FirstName, e.LastName, Shipped \
-    #    ORDER BY e.LastName, e.FirstName, NumOrders DESC")
     sqlClient.wait_for_job(jobId)
     result = sqlClient.get_result(jobId)
 
