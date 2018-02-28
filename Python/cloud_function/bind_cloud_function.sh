b2c6b6754bb38f1076f9b7b5703d48950a7eaf54
torsten@de.ibm.com <torsten@de.ibm.com>
2018-02-28 08:51:56 +0100

    Add missing copyright and comments

--- Python/cloud_function/bind_cloud_function.sh
+++ Python/cloud_function/bind_cloud_function.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+# You need to edit this script and replace the placeholders below with
+# your instance and API key data.
+
 bx wsk action update ibmcloudsql_cloudfunction \
  --param apikey <your API key here> \
  --param sqlquery_instance_crn <your SQL Query instance CRN here> \
