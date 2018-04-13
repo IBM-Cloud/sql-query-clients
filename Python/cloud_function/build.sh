0f580187db245af797d6928dd6b942f8d5d9fd42
torsten@de.ibm.com <torsten@de.ibm.com>
2018-04-13 17:41:26 +0200

    Add push to ibmfunctions/sqlquery

--- Python/cloud_function/build.sh
+++ Python/cloud_function/build.sh
@@ -1,3 +1,5 @@
 docker build --tag torsstei/sqlfunction:latest .
 docker push torsstei/sqlfunction 
+docker tag torsstei/sqlfunction:latest ibmfunctions/sqlquery:latest
+docker push ibmfunctions/sqlquery:latest
 
