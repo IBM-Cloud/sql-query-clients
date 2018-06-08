8e7c60abc72f44088cce391966862d2aef9b9854
carola girolami <carola_girolami@it.ibm.com>
2018-06-08 09:16:52 +0200

    Added results pagination.

--- Python/cloud_function/sqlfunction.py
+++ Python/cloud_function/sqlfunction.py
@@ -42,20 +42,40 @@ sql_statement_text = args.get("sql", "")
 if sql_statement_text == "":
     print({'error': 'No SQL statement specified'})
     quit()
+sql_max_results = args.get("maxresults", "")
+if sql_max_results == "":
+    print({'info': 'No max results specified'})
+sql_last_index = args.get("lastindex", "")
+if sql_last_index == "":
+    print({'info': 'No last index specified'})
+sql_job_id = args.get("jobid", "")
+if sql_job_id == "":
+    print({'info': 'No job id specified'})    
 sqlClient = ibmcloudsql.SQLQuery(ibmcloud_apikey, sql_instance_crn, target_url, client_info=client_information)
 sqlClient.logon()
-jobId = sqlClient.submit_sql(sql_statement_text)
-sqlClient.wait_for_job(jobId)
-result = sqlClient.get_result(jobId)
-result_location = sqlClient.get_job(jobId)['resultset_location']
-
+if sql_job_id == "":
+    print({'info': 'New query'})    
+    jobId = sqlClient.submit_sql(sql_statement_text)
+    sqlClient.wait_for_job(jobId)
+    if sql_max_results == "":
+        result = sqlClient.get_result(jobId)
+    else:
+        result = sqlClient.get_result(jobId).iloc[0:sql_max_results]   
+    result_location = sqlClient.get_job(jobId)['resultset_location']
+else:    
+    print({'info': 'Job id specified'})    
+    first_index = sql_last_index+1
+    new_last_index = first_index+sql_max_results   
+    result = sqlClient.get_result(sql_job_id).iloc[first_index:new_last_index]
+    jobId = sql_job_id
+    result_location = sqlClient.get_job(sql_job_id)['resultset_location'] 
 access_code = 'import ibmcloudsql\n'
 access_code += 'api_key="" # ADD YOUR API KEY HERE\n'
 access_code += 'sqlClient = ibmcloudsql.SQLQuery(api_key, ' + sql_instance_crn + ', ' + target_url + ')\n'
 access_code += 'sqlClient.logon()\n'
 access_code += 'result_df = sqlClient.get_result(' + jobId + ')\n'
 
-result_json={'jobId': jobId, 'result_location': result_location, 'result_access_pandas': access_code, 'result_set_sample': result.head(10).to_json(orient='table')}
+result_json={'jobId': jobId, 'result_location': result_location, 'result_access_pandas': access_code, 'result_set_sample': result.to_json(orient='table')}
 print(json.dumps(result_json))
 
 
