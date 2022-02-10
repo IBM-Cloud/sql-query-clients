6880072dd0d3b7ff3d7427f7ed6677f848cee03b
torsten <torsten@de.ibm.com>
2022-02-10 15:32:53 +0100

    Fixing the previous improvement :-)

--- Python/ibmcloudsql/SQLQuery.py
+++ Python/ibmcloudsql/SQLQuery.py
@@ -857,7 +857,7 @@ class SQLQuery(COSClient, SQLBuilder, HiveMetastore):
                 )
             )
 
-        if "resultset_location" not in job_details or not job_details["resultset_format"]:
+        if "resultset_location" not in job_details or "resultset_format" not in job_details:
             return None
 
         url_parsed = self.analyze_cos_url(job_details["resultset_location"])
