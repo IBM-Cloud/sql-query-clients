73e6e8a46c5b5526937f18fb6e13c1831e3d9657
torsten <torsten@de.ibm.com>
2022-04-26 08:33:29 +0200

    Updated image build for new Dremio version

--- Dremio/build_image.sh
+++ Dremio/build_image.sh
@@ -1,4 +1,6 @@
 docker build --tag dremio-ibm-sql-query .
-docker tag dremio-ibm-sql-query torsstei/dremio-ibm-sql-query
+docker tag dremio-ibm-sql-query torsstei/dremio-ibm-sql-query:dremio20.1.0-202202061055110045-36733c65
+docker tag dremio-ibm-sql-query torsstei/dremio-ibm-sql-query:latest
+docker push torsstei/dremio-ibm-sql-query:dremio20.1.0-202202061055110045-36733c65
 docker push torsstei/dremio-ibm-sql-query:latest
 
