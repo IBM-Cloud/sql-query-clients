6f01dcd1c398589c05ad30f01ffd6da3e81d3733
torsten <torsten@de.ibm.com>
2022-10-28 09:04:41 +0200

    New mod level for PR 139

--- Python/ibmcloudsql/__init__.py
+++ Python/ibmcloudsql/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 # ------------------------------------------------------------------------------
 
-__version__ = "0.5.10"
+__version__ = "0.5.11"
 # flake8: noqa F401
 from .SQLQuery import SQLQuery
 from .sql_query_ts import SQLClientTimeSeries
